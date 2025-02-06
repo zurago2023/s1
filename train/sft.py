82ca2d3e095cdffb5e245083a8fa671cebce349a
Niklas Muennighoff <n.muennighoff@gmail.com>
2025-02-05 17:40:16 -0800

    Fix path

--- train/sft.py
+++ train/sft.py
@@ -17,7 +17,7 @@ class TrainingConfig:
     block_size: int = field(default=32768)
     wandb_project: Optional[str] = field(default="s1")
     wandb_entity: Optional[str] = field(default="hashimoto-group")
-    train_file_path: Optional[str] = field(default='s1/s1K_tokenized')
+    train_file_path: Optional[str] = field(default='simplescaling/s1K_tokenized')
     dagger: bool = field(default=False)
 
     def __post_init__(self):
